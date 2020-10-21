39afef42208ecfc80eb2c047ec3a24159dc3458c
VinamraGupta <60665195+VinamraGuptaa@users.noreply.github.com>
2020-10-21 23:21:55 +0530

    Add files via upload
    Learned about how to use Glide library and how to add profile picture

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,76 +1,125 @@
-package com.example.projectmanag
+package com.projemanag.activities
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.projemanag.R
+import com.projemanag.firebase.FirestoreClass
+import com.projemanag.model.User
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : BaseActivity(),NavigationView.OnNavigationItemSelectedListener {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupActionBar()
-        nav_View.setNavigationItemSelectedListener(this)
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_main)
+
+        setupActionBar()
+
+        // Assign the NavigationView.OnNavigationItemSelectedListener to navigation view.
+        nav_view.setNavigationItemSelectedListener(this)
+
+        // TODO (Step 3: Call a function to get the current logged in user details.)
+        // START
+        // Get the current logged in user details.
+        FirestoreClass().signInUser(this@MainActivity)
+        // END
     }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            // A double back press function is added in Base Activity.
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_my_profile -> {
+
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.nav_sign_out -> {
+                // Here sign outs the user from firebase in this device.
+                FirebaseAuth.getInstance().signOut()
+
+                // Send the user to the intro screen of the application.
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    /**
+     * A function to setup action bar
+     */
     private fun setupActionBar() {
 
         setSupportActionBar(toolbar_main_activity)
-
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
-        }
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
 
         toolbar_main_activity.setNavigationOnClickListener {
             toggleDrawer()
         }
     }
 
-    private fun toggleDrawer(){
-        if(Drawer_Layout.isDrawerOpen(GravityCompat.START)){
-                Drawer_Layout.closeDrawer(GravityCompat.START)
-            }
-            else{
-            Drawer_Layout.openDrawer(GravityCompat.START)
+    /**
+     * A function for opening and closing the Navigation Drawer.
+     */
+    private fun toggleDrawer() {
 
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
         }
     }
 
-    override fun onBackPressed() {
-        if(Drawer_Layout.isDrawerOpen(GravityCompat.START)){
-            Drawer_Layout.closeDrawer(GravityCompat.START)
-        }
-        else{
-           doubleBackToExit()
-        }
+    // TODO (Step 5: Create a function to update the user details in the navigation view.)
+    // START
+    /**
+     * A function to get the current user details from firebase.
+     */
+    fun updateNavigationUserDetails(user: User) {
+        // The instance of the header view of the navigation view.
+        val headerView = nav_view.getHeaderView(0)
 
+        // The instance of the user image of the navigation view.
+        val navUserImage = headerView.findViewById<ImageView>(R.id.iv_user_image)
+
+        // Load the user image in the ImageView.
+        Glide
+            .with(this@MainActivity)
+            .load(user.image) // URL of the image
+            .centerCrop() // Scale type of the image.
+            .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+            .into(navUserImage) // the view in which the image will be loaded.
+
+        // The instance of the user name TextView of the navigation view.
+        val navUsername = headerView.findViewById<TextView>(R.id.tv_username)
+        // Set the user name
+        navUsername.text = user.name
     }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.nav_my_profile->{
-                Toast.makeText(this,"Go to Profile",Toast.LENGTH_SHORT).show()
-            }
-            R.id.nav_sign_out->{
-                FirebaseAuth.getInstance().signOut()
-                val intent= Intent(this,Intro_Activity::class.java)
-                startActivity(intent)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                finish()
-            }
-
-
-        }
-        Drawer_Layout.closeDrawer(GravityCompat.START)
-        return true
-
-    }
-
+    // END
 }
